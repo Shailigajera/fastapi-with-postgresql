70dc3858cfb85608b51c0ddcde061e442fded694
shaili gajera <shailigajera@gmail.com>
2025-06-04 17:25:49 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import  FastAPI , Depends, HTTPException, status
+from fastapi import  FastAPI , Depends, HTTPException, status, Response
 import services, model, schemas
 from db import get_db, engine
 from sqlalchemy.orm import Session
@@ -20,4 +20,16 @@ def get_book_by_id(id: int, db: Session = Depends(get_db)):
     if queryset :
         return queryset
     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+
+@app.put('/books/{id}', response_model=schemas.Book)
+def update_details(book: schemas.BookCreate , id: int, db : Session = Depends(get_db)):
+    return services.update_book(db, book, id)
+
+@app.delete('/books/{id}', response_model = schemas.Book)
+def delete_book(id:int , db :Session = Depends(get_db)):
+    delete_detail = services.delete_book(db, id)
+    if delete_detail:
+        return Response(status_code = status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, details= 'Book not Found')
+
 
--- services.py
+++ services.py
@@ -16,3 +16,22 @@ def get_book(db:Session):
 
 def get_books(db:Session , id :int):
     return db.query(Book).filter(Book.id==id).first()
+
+def update_book(db : Session, book: BookCreate,  id :int):
+    book_details =  db.query(Book).filter(Book.id==id).first()
+    if book_details:
+        for key,value in book.model_dump().items():
+            setattr(book_details, key, value)
+    db.commit()
+    db.refresh(book_details)
+    return book_details
+
+def delete_book(db:Session , id:int):
+    book_detail = db.query(Book).filter(Book.id==id).first()
+    if book_detail :
+            db.delete(book_detail)
+            db.commit()
+    return (book_detail)
+             
+
+
